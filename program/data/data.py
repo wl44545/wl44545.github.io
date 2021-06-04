2963973001dd95442c3ff9bf20bb6e6097e4d647
Lukasz Wieckowski <72666991+wl44545@users.noreply.github.com>
2021-06-04 15:30:14 +0200

    Close #2 Close #4

--- program/data/data.py
+++ program/data/data.py
@@ -1,7 +1,12 @@
 """
 Moduł zawierający obsługę danych źródłowych.
 """
+import os
 
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle as pkl
 
 class Data(object):
 	"""
@@ -11,5 +16,33 @@ class Data(object):
 		"""
 		Konstruktor.
 		"""
-		self.covid = None
-		self.normal = None
+		self.class_names = ['normal', 'pneumonia', 'covid']
+		self.images = []
+		self.labels = []
+
+	def import_data(self):
+		for entry in os.scandir(r'resources\raw_data\normal'):
+			if entry.path.endswith(".png") and entry.is_file():
+				image = plt.imread(entry.path)
+				self.images.append(image)
+				self.labels.append(0)
+		for entry in os.scandir(r'resources\raw_data\pneumonia'):
+			if entry.path.endswith(".png") and entry.is_file():
+				image = plt.imread(entry.path)
+				self.images.append(image)
+				self.labels.append(1)
+		for entry in os.scandir(r'resources\raw_data\covid'):
+			if entry.path.endswith(".png") and entry.is_file():
+				image = plt.imread(entry.path)
+				self.images.append(image)
+				self.labels.append(2)
+
+
+	def dump_data(self):
+		file_images = open('resources\pickled_data\images_pickled', 'wb')
+		file_labels = open('resources\pickled_data\labels_pickled', 'wb')
+		pkl.dump(self.images, file_images, protocol=2)
+		pkl.dump(self.labels, file_labels, protocol=2)
+		file_images.close()
+		file_labels.close()
+
---
+++ program/main.py
@@ -0,0 +1,17 @@
+# import matplotlib.pyplot as plt
+#
+# import program.data
+#
+# xxx = program.data.Data()
+# xxx.load()
+#
+# img = xxx.images[0]
+#
+# plt.imshow(img)
+# plt.show()
+#
+#
+# img = xxx.images[-1]
+#
+# plt.imshow(img)
+# plt.show()
