9bb6b8f4601e941ec2ad964f3af08af1f88fd53a
Lukasz Wieckowski <72666991+wl44545@users.noreply.github.com>
2021-06-04 14:03:05 +0200

    Close #10 - klasa + konstruktor

--- program/measuring_quality/measuring_quality.py
+++ program/measuring_quality/measuring_quality.py
@@ -3,7 +3,26 @@ Moduł zawierający miary jakości klasyfikacji.
 """
 
 
-class MeasuringQuality(object):
+class MeasuringQuality:
 	"""
 	Miary jakości klasyfikacji.
 	"""
+	def __init__(self):
+		"""
+		Konstruktor.
+		"""
+		self.true_positive = 0
+		self.true_negative = 0
+		self.false_positive = 0
+		self.false_negative = 0
+		self.recall = 0
+		self.sensitivity = 0
+		self.specificity = 0
+		self.fall_out = 0
+		self.precision = 0
+		self.accuracy = 0
+		self.error = 0
+		self.f1 = 0
+
+		self.confusion_matrix = None
+		self.roc_curve = None
