57ba984ec95fea6d33afc08e35ae903d464a5522
Lukasz Wieckowski <72666991+wl44545@users.noreply.github.com>
2021-06-26 03:07:02 +0200

    some dome

--- program/main.py
+++ program/main.py
@@ -14,25 +14,25 @@ from program.algorithms.boosting.gradient_boost import GradientBoost
 
 
 data = Data()
-data.load_data(100)
-data.split_data()
 
-print(len(data.X_train),len(data.X_test),len(data.y_train),len(data.y_test))
+data.import_data(30)
+data.dump_data(30)
+
+data.load_data(30)
+data.split_data()
 
 statistics = Statistics()
 
+statistics.insert(BernoulliNBC(data).start())
+statistics.insert(ComplementNBC(data).start())
+statistics.insert(GaussianNBC(data).start())
+statistics.insert(MultinomialNBC(data).start())
 
-# statistics.insert(BernoulliNBC(data).start())
-# statistics.insert(ComplementNBC(data).start())
-# statistics.insert(GaussianNBC(data).start())
-# statistics.insert(MultinomialNBC(data).start())
-#
-# statistics.insert(LinearSVM(data).start())
-# statistics.insert(NonLinearSVM(data).start())
+statistics.insert(LinearSVM(data).start())
+statistics.insert(NonLinearSVM(data).start())
 
 statistics.insert(AdaBoost(data).start())
 statistics.insert(GradientBoost(data).start())
-
 
 statistics.create_statistics()
 statistics.show()
